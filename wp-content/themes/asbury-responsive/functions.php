d7fc56cf174b6672a63444ac1231698924c62917
Kory Northrop <korynorthrop@gmail.com>
2013-09-20 17:56:31 -0700

    Building theme

--- wp-content/themes/asbury-responsive/functions.php
+++ wp-content/themes/asbury-responsive/functions.php
@@ -8,4 +8,19 @@
      global $wp_rewrite;
      $wp_rewrite->set_permalink_structure('/%postname%/');
  }
+ 
+ 
+ /* Change logo for client login */
+ add_action("login_head", "client_login_head");
+ function client_login_head() {
+ 	echo "
+ 	<style>
+ 	body.login #login h1 a {
+ 		background: url('".get_stylesheet_directory_uri()."/images/logo-login.png') no-repeat scroll center top transparent;
+ 		height: 200px;
+ 		width: 100%;
+ 	}
+ 	</style>
+ 	";
+ }
 
