c5c1d9cd06fdab2e7f7fc81a8a738af7b7e15fb1
Kory Northrop <korynorthrop@gmail.com>
2014-03-31 17:41:24 -0700

    About page and team pages

--- wp-content/themes/asbury-responsive/page.php
+++ wp-content/themes/asbury-responsive/page.php
@@ -34,6 +34,42 @@ get_header(); ?>
 					<div class="entry-content">
 						<?php the_content(); ?>
 						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+						
+						<?php if(is_page('about')) : ?>
+						<ul id="about-team-nav">
+							<?php $loop = new WP_Query( array( 'post_type' => 'page',
+														        'meta_query' => array(
+														            array(
+														                'key' => '_wp_page_template',
+														                'value' => 'page-templates/team.php',
+														            )
+														        ),
+																'posts_per_page' => -1,
+																'orderby' => 'menu_order',
+																'order' => 'ASC'
+							) ); ?>
+							<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+								<li class=''>
+									<a href="<?php echo the_permalink(); ?>"><img class="thumb" src="<?php the_field('team_thumbnail'); ?>" /></a>
+									<a href="<?php echo the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+									<span><?php if(get_field('linked_in')) the_field('team_title'); ?></span>
+									<div class="contact-icons">
+										<?php if(get_field('linked_in')) : ?>
+										<a class="team-icon" href="<?php the_field('linked_in'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/team-linkedin.png" alt="LinkeIn Icon" /></a>
+										<?php endif; ?>
+										<?php if(get_field('twitter')) : ?>
+										<a class="team-icon" href="<?php the_field('twitter'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/team-twitter.png" alt="Twitter Icon" /></a>
+										<?php endif; ?>
+										<?php if(get_field('team_email')) : ?>
+										<a class="team-icon" href="mailto:<?php the_field('team_email'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/team-email.png" alt="Email Icon" /></a>
+										<?php endif; ?>
+									</div>
+								</li>
+							<?php endwhile; ?>
+							<?php wp_reset_query();  // Restore global post data stomped by the_post(). ?>
+						</ul>
+						<? endif; ?>
+
 					</div><!-- .entry-content -->
 
 					<footer class="entry-meta">
