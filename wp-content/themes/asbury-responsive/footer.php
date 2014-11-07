028b19b90edcc2331cb27449d5600a6996a07e05
Kory Northrop <korynorthrop@gmail.com>
2014-11-07 13:43:55 -0800

    New footer

--- wp-content/themes/asbury-responsive/footer.php
+++ wp-content/themes/asbury-responsive/footer.php
@@ -14,23 +14,13 @@
 		</div><!-- #main -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="footer-container">
-				<div id="icons" class="left">
-					<a href="https://twitter.com/AsburyDesign"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.jpg" alt="" /></a>
-					<a href="https://www.facebook.com/AsburyDesign"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/facebook.jpg" alt="" /></a>
-					<a href="http://vimeopro.com/asburydesign/asbury-design/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/vimeo.jpg" alt="" /></a>
-					<a href="https://www.pinterest.com/asburydesign/asbury-design-interiors/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pinterest.jpg" alt="" /></a>
-					<a href="http://www.houzz.com/pro/asburydesign/asbury-design"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/houzz.jpg" alt="" /></a>
-					<a href="mailto:steven@asburydesign.net"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/mail.jpg" alt="" /></a>
-				</div>
-				<div id="footer-contact">
-					1603 East 22nd Avenue<br />
-					Eugene, OR 97403<br />
-					<span class="contact-tel">p. 541-344-1633</span>
-				</div>
-				<div id="copyright">
-					&copy; <?php echo date('Y'); ?> Asbury Design
-				</div>
-				<div class="clear"></div>
+				<?php if ( is_active_sidebar( 'footer-widget' ) ) : ?>
+					<div class="wrap">
+						<?php dynamic_sidebar( 'footer-widget' ); ?>
+						<div class="back-to-top"><a href="#page">Back to Top</a></div>
+					</div>
+					<div class="clear"></div>
+				<?php endif; ?>
 			</div><!-- .footer-container -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
