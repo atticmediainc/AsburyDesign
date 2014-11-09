12a2b4a4adf812c24851c6a338d24bf654c7b2ea
Russell Kaye <russell@atticmediainc.com>
2014-11-09 13:02:38 -0800

    Upgrading WP to 4.0

--- wp-config.php
+++ wp-config.php
@@ -15,16 +15,27 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
+
 /** The name of the database for WordPress */
-define('DB_NAME', 'asburydesign');
+
+define('DB_NAME', 'atticmed_asburydesign');
+
+
 
 /** MySQL database username */
+
 define('DB_USER', 'root');
 
+
+
 /** MySQL database password */
+
 define('DB_PASSWORD', 'root');
 
+
+
 /** MySQL hostname */
+
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
